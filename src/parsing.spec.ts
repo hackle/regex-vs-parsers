be95df8adb1eec0b8614849b909e80e612985354
Hacke Wayne <hacklew@hotmail.com>
2020-09-29 19:44:56 +1300

    init

---
+++ src/parsing.spec.ts
@@ -0,0 +1,40 @@
+import { Mode } from './modes';
+import { parseIt } from './parsing';
+
+const positives: Record<string, Mode> = {
+    '/book': { mode: 'create' },
+    '/book/': { mode: 'create' },
+    '/book/123': { mode: 'edit', id: '123' },
+    '/book/123/': { mode: 'edit', id: '123' },
+    '/book/123/clone': { mode: 'clone', id: '123' },
+    '/book/123/clone/': { mode: 'clone', id: '123' }
+};
+
+const negatives = [
+    '/book//',
+    '/book123',
+    '/bookclone',
+    '/book123clone',
+    '/book123/clone'
+];
+
+describe('With positive cases', () => {
+    for (const url in positives) {
+        it(`Parsing ${url}`, () => {
+            const expected = positives[url];
+            const actual = parseIt(url);
+
+            expect(actual).toEqual(expected);
+        });
+    }
+});
+
+describe('With negative cases', () => {
+    for (const url of negatives) {
+        it(`Parsing ${url}`, () => {
+            const actual = parseIt(url);
+
+            expect(actual).toBeNull();
+        });
+    }
+});
---
+++ src/parsing.ts
@@ -0,0 +1,25 @@
+import * as P from 'parsimmon';
+import { guid } from './guid';
+import { Mode } from './modes';
+
+const slash = P.string('/');
+
+const bookId = guid; 
+// const bookId = P.digit.atLeast(1).tie();
+const edit = slash.then(bookId);
+const clone = edit.skip(slash).skip(P.string('clone'));
+
+const urlParser: P.Parser<Mode> = 
+    P.string('/book')
+    .then(P.alt(
+            clone.map<Mode>(id => ({ id, mode: 'clone' })), 
+            edit.map<Mode>(id => ({ id, mode: 'edit' })),
+            P.of<Mode>({ mode: 'create' })
+        )
+    )
+    .skip(slash.or(P.of(null)));
+
+export function parseIt(url: string): Mode | null {
+    const result = urlParser.parse(url);
+    return result.status ? result.value : null;
+}

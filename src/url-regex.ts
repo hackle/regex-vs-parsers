5746742ad2660499667f252e21d5e8c41f0db6dc
Hacke Wayne <hacklew@hotmail.com>
2020-10-11 18:51:26 +1300

    comment

--- src/url-regex.ts
+++ src/url-regex.ts
@@ -6,6 +6,7 @@ export function parseIt(url: string): Mode | null {
     const match = regex.exec(url);
     if (!match) return null;
 
+    // at 0 is the whole string is matches
     const [, id, clone ] = match;
 
     if (clone) return { mode: 'clone', id: id };

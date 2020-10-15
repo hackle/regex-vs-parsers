caac032496030cf41ef344673c153cc1ed03cd69
Hacke Wayne <hacklew@hotmail.com>
2020-10-15 14:20:28 +1300

    url parser reset

--- src/3.url-parser.ts
+++ src/3.url-parser.ts
@@ -1,7 +1,7 @@
 import * as P from 'parsimmon';
 import { Mode } from './0.modes';
 
-const urlParser: P.Parser<Mode> = P.of<Mode>({ mode: 'create' });
+const urlParser: P.Parser<Mode> = P.of<Mode>({} as Mode);
 
 export function parseIt(url: string): Mode | null {
     const result = urlParser.parse(url);
